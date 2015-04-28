50e01421b8af3796d894b45d841fcb7b50344d31
Mark Macdonald <mjemacdonald@gmail.com>
2015-04-27 17:00:57 -0700

    (feat) Initial jumbotron styling

--- public/client/views/JumbotronView.js
+++ public/client/views/JumbotronView.js
@@ -6,7 +6,7 @@ var JumbotronView = Backbone.View.extend({
     this.render();
   },
 
-  template: _.template('<h1>SENTIMENTAL</h1>'),
+  template: _.template('<h1>SENTIMENTAL</h1><p>_____________________</p><h3>Space for a Tagline Because Ya Know</h3>'),
 
   render: function(){
     return this.$el.html(this.template());
